7505c36ec75c3ec8defb8e96126ea181e8c0320c
Vanessa Stewart <vstewart1213@gmail.com>
2021-01-10 12:53:21 -0800

    Add routes for posting, getting, and deleting individual blogs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
+const { render } = require('ejs');
 
 // create express app
 const app = express();
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');  // looks in views folder by default
 
 //middleware and static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // mongoose and mongo sandbox routes
@@ -80,6 +82,43 @@ app.get('/blogs', (req, res) => {
 app.get('/blogs/create', (req,res) => {
   res.render('create', {title: "Create a New Blog"})
 })
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs')
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
+
+
 
 // 404
 app.use((req, res) => {
